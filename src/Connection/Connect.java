d5c9ed1e0baf45bbd4943feb0b7551019a5cc07e
Hoang12342906 <104152244+Hoang12342906@users.noreply.github.com>
2022-05-12 11:06:49 +0700

    first commit

---
+++ src/Connection/Connect.java
@@ -0,0 +1,25 @@
+package Connection;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author 
+ */
+public class Connect {
+    
+    public static Connection ConnectSQL (){
+        String url = "net.sourceforge.jtds.jdbc.Driver";
+        try {
+            Class.forName(url);
+            String dbUrl = "jdbc:jtds:sqlserver://LAPTOP-6IBE59SJ:1433/QuanLiBaoHiem;instance=SQLEXPRESS;user=sa;password=123456789";
+            return DriverManager.getConnection(dbUrl);
+           
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
